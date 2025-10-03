39af7a87e53b4b0273e0e78473fe944b9f8a4684
mrmalvi <malviyak00@gmail.com>
2025-10-03 18:10:43 +0530

    fix rb

--- lib/deadweight_rails/ruby_analyzer.rb
+++ lib/deadweight_rails/ruby_analyzer.rb
@@ -15,7 +15,7 @@ module DeadweightRails
         current_class = nil
         content = File.read(file)
 
-        content.each_line do |line|
+        content.each_line.with_index do |line, idx|
           # Detect class definitions
           if line =~ /^\s*class\s+([\w:]+)/
             current_class = $1
@@ -23,13 +23,18 @@ module DeadweightRails
 
           # Detect method definitions
           if line =~ /^\s*def\s+([\w\?\!]+)/
-            classes[current_class] << $1 if current_class
+            classes[current_class] << { name: $1, line: idx + 1 } if current_class
           end
 
-          # Detect method calls (simple: .method_name or method_name())
           next if current_class.nil?
-          classes[current_class].each do |method|
-            usages[current_class] << method if line.match?(/(\.| )#{Regexp.escape(method)}(\(|\s)/)
+
+          # Detect method usage (simplified)
+          classes[current_class].each do |m|
+            method_name = m[:name]
+            # Match `.method` or `method(` or `self.method` but ignore definition line
+            if idx + 1 != m[:line] && line.match?(/(\.|self\.)#{Regexp.escape(method_name)}(\(|\s)/)
+              usages[current_class] << method_name
+            end
           end
         end
       end
